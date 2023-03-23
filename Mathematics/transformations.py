bfb0febf09d2471a221e937610d0b1937380021d
Nemalger <126774947+Nemalger@users.noreply.github.com>
2023-03-23 22:12:28 +0300

    Add files via upload

---
+++ Mathematics/transformations.py
@@ -0,0 +1,31 @@
+from copy import copy
+import yaml
+from pprint import pprint
+class Physics:
+
+    def __init__(self):
+        with open('Config.YAML') as f:
+            templates = yaml.safe_load(f)
+        self.all_constants = templates['Base const']
+        variable_parameters = templates['Variable parameters']
+        self.functions = templates['Functions']
+        for i in variable_parameters:
+            self.all_constants[i] = eval(variable_parameters[i], copy(self.all_constants))
+
+    def get_constant(self, name):
+        return self.all_constants[name]
+
+    def get_function(self, name_f, **argument):
+        self.all_constants.update(argument)
+        function = self.functions[name_f]
+        return eval(function['formula'], self.all_constants)
+
+
+physics = Physics()
+print(physics.get_function('food', t = 31536000, p = 8))
+
+
+
+
+
+
