abc90fc727d45422bbe4a765a8a82c8cafadce23
durenbrudenb <127397509+durenbrudenb@users.noreply.github.com>
2023-03-23 22:30:40 +0300

    Добавлены часы

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+from kivy.clock import Clock
 from kivy.app import App
 from kivy.lang import Builder
 from windows import *
 kv = Builder.load_file('windows.kv')
 
+FPS = 60
+
 class GameApp(App):
     def build(self, **kwargs):
         return kv
+    def on_start(self):
+        self.time = 0
+
+    def update(self, dt):
+        self.time += dt
+        self.root.ids['game_window'].ids['clock'].text = f"{self.time: .2f}"
+
+
 
 if __name__ == '__main__':
-    GameApp().run()
+    app = GameApp()
+    Clock.schedule_interval(app.update, 1/FPS)
+    app.run()
